7dd2a9c8e6a45633f9be085e791aa12ad1992668
chiragshahklc <chiragshahklc@gmail.com>
2017-07-13 09:59:15 +0530

    I tried to comment into the Form1.cs in AnonymousServer

--- AnonymousServer/Form1.cs
+++ AnonymousServer/Form1.cs
@@ -24,6 +24,7 @@ namespace AnonymousServer
             db.Add(new FileDbStructure("Chirag Shah","Chirag", "Shah", Usertype.Manager));
             if (f1.WriteToDbFile(db))
             {
+                //Let's try to comment this out.
                 MessageBox.Show("Success");
             }
             else
